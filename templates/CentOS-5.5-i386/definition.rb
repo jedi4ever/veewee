c3d949f5d07a0ae686a178a5ae61e3c1bb97e540
Chris Adams <wave@chrisadams.me.uk>
2010-11-18 00:46:12 +0000

    Switched these CentOS files around, and added in details for a netboot. CentOS's download disc is preposterously big to download.

--- templates/CentOS-5.5-i386/definition.rb
+++ templates/CentOS-5.5-i386/definition.rb
@@ -1,16 +1,17 @@
-       Veewee::Session.declare( {
-        :cpu_count => '1', :memory_size=> '384', 
-        :disk_size => '10140', :disk_format => 'VDI',:disk_size => '10240' ,
-        :os_type_id => 'RedHat',
-        :iso_file => "CentOS-4.8-i386-bin-DVD.iso", :iso_src => "", :iso_md5 => "", :iso_download_timeout => 1000,
-        :boot_wait => "10",:boot_cmd_sequence => [ 
-          		          'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>' 				  
-          ],
-        :kickstart_port => "7122", :kickstart_timeout => 10000,:kickstart_file => "ks.cfg",
-        :ssh_login_timeout => "100",:ssh_user => "vagrant", :ssh_password => "vagrant",:ssh_key => "",
-        :ssh_host_port => "2222", :ssh_guest_port => "22",
-        :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
-        :shutdown_cmd => "/sbin/halt -h -p",
-        :postinstall_files => [ "postinstall.sh"],:postinstall_timeout => 10000
-         }
-      )
+Veewee::Session.declare( {
+:cpu_count => '1', :memory_size=> '384', 
+:disk_size => '10140', :disk_format => 'VDI',:disk_size => '10240' ,
+:os_type_id => 'RedHat',
+:iso_file => "CentOS-5.5-i386-netinstall.iso", 
+:iso_src => "http://mirror.bytemark.co.uk/centos/5.5/isos/i386/CentOS-5.5-i386-netinstall.iso", 
+:iso_md5 => "0172883a3039772165db073693debae5", 
+:iso_download_timeout => 1000,
+:boot_wait => "10",:boot_cmd_sequence => [ 'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
+:kickstart_port => "7122", :kickstart_timeout => 10000,:kickstart_file => "ks.cfg",
+:ssh_login_timeout => "100",:ssh_user => "vagrant", :ssh_password => "vagrant",:ssh_key => "",
+:ssh_host_port => "2222", :ssh_guest_port => "22",
+:sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+:shutdown_cmd => "/sbin/halt -h -p",
+:postinstall_files => [ "postinstall.sh"],:postinstall_timeout => 10000
+}
+)
