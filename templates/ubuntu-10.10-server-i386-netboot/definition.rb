4b840cdef89454a3b95725926bafcf1b978bc1fa
Patrick Debois <Patrick.Debois@jedi.be>
2011-02-08 09:52:17 +0100

    minimal chef install + extra templates for ubuntu netboot

---
+++ templates/ubuntu-10.10-server-i386-netboot/definition.rb
@@ -0,0 +1,25 @@
+Veewee::Session.declare( {
+  :cpu_count => '1', :memory_size=> '256', 
+  :disk_size => '10140', :disk_format => 'VDI',:disk_size => '10240' ,
+  :os_type_id => 'Ubuntu',
+  :iso_file => "ubuntu-10.10-server-i386-netboot.iso", 
+  :iso_src => "http://archive.ubuntu.com/ubuntu/dists/maverick/main/installer-i386/current/images/netboot/mini.iso",
+  :iso_md5 => "02abb1a71bde21a1335e9368dad529ca",
+  :iso_download_timeout => "1000",
+  :boot_wait => "10",:boot_cmd_sequence => [ 
+                 '<Tabr>',
+    		         'noapic preseed/url=http://%IP%:%PORT%/preseed.cfg ',
+    		         'debian-installer=en_US auto locale=en_US kbd-chooser/method=us ',
+   				       'hostname=%NAME% ',
+    		         'fb=false debconf/frontend=noninteractive ',
+   		           'console-setup/ask_detect=false console-setup/modelcode=pc105 console-setup/layoutcode=us ',
+    		         ' -- <Enter>' 
+    ],
+  :kickstart_port => "7122", :kickstart_timeout => "10000",:kickstart_file => "preseed.cfg",
+  :ssh_login_timeout => "10000",:ssh_user => "vagrant", :ssh_password => "vagrant",:ssh_key => "",
+  :ssh_host_port => "2222", :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "shutdown -P now",
+  :postinstall_files => [ "postinstall.sh"],:postinstall_timeout => "10000"
+   }
+)
