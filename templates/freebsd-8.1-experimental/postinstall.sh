100075a1c98b299cb66b014060356118cb38f58b
Patrick Debois <Patrick.Debois@jedi.be>
2011-02-28 08:19:20 +0100

    some progress on freebsd

--- templates/freebsd-8.1-experimental/postinstall.sh
+++ templates/freebsd-8.1-experimental/postinstall.sh
@@ -1,9 +1,30 @@
 #http://www.freebsd.org/doc/en_US.ISO8859-1/articles/remote-install/installation.html
 dd if=/dev/zero of=/dev/ad4 count=2
-#bsdlabel -wB /dev/ad4
+#bsdlabel -w -B /dev/ad4
 
 sysctl kern.geom.debugflags=16
 
+#http://forum.nginx.org/read.php?23,146311,146451
+# Init the disk with an MBR
+gpart create -s mbr ad4 
+# Create a BSD container
+gpart add -t freebsd ad4 
+# Init with a BSD scheme
+gpart create -s bsd ad4s1 
+
+# 1GB for /
+gpart add -t freebsd-ufs -s 1G ad4s1 
+
+gpart add -t freebsd-swap -s 2G ad4s1 
+# 2GB for swap
+gpart add -t freebsd-ufs -s 2G ad4s1 
+# 2GB for /var
+gpart add -t freebsd-ufs ad4s1 
+# all rest for /usr
+
+#install bootcode
+gpart bootcode -p /boot/boot -i 1 ad4s1
+gpart set -a active -i 	ad4
 
 cat <<EOF >/install.cfg
 # This is the installation configuration file for our rackmounted FreeBSD 
@@ -28,9 +49,14 @@ tryDHCP=NO
 
 ################################
 # Which installation device to use 
-_ftpPath=ftp://ftp.freebsd.org/pub/FreeBSD/
+# ftp://ftp.smr.ru/pub/0/FreeBSD/current/src/release/sysinstall/sysinstall.h
+_ftpPath=ftp://ftp2.freebsd.org/pub/FreeBSD/
+#_httpPath=ftp://ftp2.freebsd.org/pub/FreeBSD/
+#httpProxy=192.168.2.10:800
 netDev=em0
 mediaSetFTP
+#mediaSetHTTP
+
 ################################
 
 ################################
@@ -47,7 +73,7 @@ partition=all
 #http://www.mail-archive.com/freebsd-questions@freebsd.org/msg212036.html
 bootManager=standard
 diskPartitionEditor
-diskPartitionWrite
+#diskPartitionWrite
 
 ################################
 
@@ -92,5 +118,72 @@ installCommit
 
 EOF
 
-exit
 sysinstall configFile=/install.cfg loadConfig
+
+
+#http://www.daemonforums.org/showthread.php?t=4389
+
+cd /mnt/boot
+cp -Rp GENERIC/* kernel/
+
+#For some reason this is the disk layout the sysinstall creates
+
+cat <<EOF > /mnt/etc/fstab
+/dev/ad4s1b  none            swap    sw              0       0
+/dev/ad4s1d / ufs rw 0 0
+/dev/ad4s1f /usr ufs rw 0 0
+/dev/ad4s1e /var ufs rw 0 0
+EOF
+
+
+# boot0cfg -B ad4
+#make the menu appear
+
+# Booting mentions an error
+# Invalid partition
+# FAILS WITH: 0:ad(0,a)/boot/kernel/kernel
+# WORKS WITH: 0:ad(4,d)/boot/loader
+
+#http://www.mail-archive.com/freebsd-questions@freebsd.org/msg72530.html
+cat  <<EOF > /mnt/boot/loader.conf 
+#geom_label_load="YES"
+#root_disk_unit="4"
+#currdev="disk4s1d"
+#rootdev="disk4s1d"
+#vfs.root.mountfrom="ufs:/dev/ad4s1d"
+EOF
+
+#activate dhcp
+cat <<EOF > /mnt/etc/rc.conf
+ifconfig_DEFAULT="DHCP"
+EOF
+
+
+#The vi edit program needs a /var/tmp/vi.recover file.
+
+mkdir -p /mnt/usr/local/etc/rc.d/
+cd /mnt/usr/local/etc/rc.d/
+cat  <<EOF > mkvirecover
+#!/bin/sh
+# PROVIDE: mkvirecover
+# REQUIRE: mountcritremote
+# BEFORE: DAEMON virecover
+ 
+
+. /etc/rc.subr
+ 
+name="mkvirecover"
+stop_cmd=":"
+start_cmd="mkvirecover_start"
+ 
+mkvirecover_start()
+{
+[ -d /var/tmp/vi.recover ] || mkdir -m 1777 /var/tmp/vi.recover
+echo '.'
+}
+ 
+load_rc_config "\$name"
+run_rc_command "\$1"
+EOF
+ 
+chmod 555 /mnt/usr/local/etc/rc.d/mkvirecover
