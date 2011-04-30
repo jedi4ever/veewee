59754ae691caa248cf6eb8200989d54633f1bafa
Sean Plaice <splaice@gmail.com>
2011-04-29 22:02:57 -0700

    add not netboot templates and add 64bit templates for fedora 14

---
+++ templates/Fedora-14-amd64-netboot/definition.rb
@@ -0,0 +1,16 @@
+Veewee::Session.declare({
+  :cpu_count => '1', :memory_size=> '384',
+  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off', :use_hw_virt_ext => 'on',
+  :os_type_id => 'Fedora',
+  :iso_file => "Fedora-14-x86_64-netinst.iso",
+  :iso_src => "http://mirror.uoregon.edu/fedora/linux/releases/14/Fedora/x86_64/iso/Fedora-14-x86_64-netinst.iso",
+  :iso_md5 => "acd25fc1470f2497cf2a2a245adbfe1e",
+  :iso_download_timeout => 1000,
+  :boot_wait => "10", :boot_cmd_sequence => [ '<Tab> linux text ks=http://%IP%:%PORT%/ks.cfg<Enter><Enter>' ],
+  :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "ks.cfg",
+  :ssh_login_timeout => "100", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
+  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => 10000
+})
