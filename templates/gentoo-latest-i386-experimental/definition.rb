f90b6234c5a633c2bb55d17f4f35f6d8352925ee
Patrick Debois <Patrick.Debois@jedi.be>
2011-05-08 21:46:20 +0200

    First attempt at getting gentoo going.... Work in Progress

---
+++ templates/gentoo-latest-i386-experimental/definition.rb
@@ -0,0 +1,29 @@
+Veewee::Session.declare( {
+  :cpu_count => '1', :memory_size=> '768',
+  :disk_size => '10140', :disk_format => 'VDI',:hostiocache => 'off',
+  :os_type_id => 'Gentoo',
+  :iso_file => "install-x86-minimal-20110426.iso",
+  :iso_src => "http://distfiles.gentoo.org/releases/x86/autobuilds/current-iso/install-x86-minimal-20110426.iso",
+  :iso_md5 => "548646b9d1042a162f769bc280149417",
+  :iso_download_timeout => "1000",
+  :boot_wait => "120",:boot_cmd_sequence => [
+        'net-setup eth0<Enter>',
+        '<Wait><Enter>',
+        '2<Enter>',
+        '1<Enter>',
+	'<Wait><Wait>ifconfig -a <Enter>',
+	'sleep 5 ;curl http://%IP%:%PORT%/stages.sh -o stages.sh &&',
+	'bash stages.sh &<Enter>',
+        #'/etc/init.d/sshd start<Enter>',
+        #'passwd<Enter><Wait><Wait>',
+	#'vagrant<Enter><Wait>',
+	#'vagrant<Enter><Wait>',
+    ],
+  :kickstart_port => "7122", :kickstart_timeout => "10000",:kickstart_file => "stages.sh",
+  :ssh_login_timeout => "10000",:ssh_user => "vagrant", :ssh_password => "vagrant",:ssh_key => "",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :sudo_cmd => "cat '%f'|su -",
+  :shutdown_cmd => "shutdown -p now",
+  :postinstall_files => [ "postinstall.sh"],:postinstall_timeout => "10000"
+   }
+)
