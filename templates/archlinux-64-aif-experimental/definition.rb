d793bd3ca6b8a82ce3917736776404dee9529e79
Patrick Debois <Patrick.Debois@jedi.be>
2011-06-07 08:51:06 +0200

    Example for Dave Simons to get started on Arch AIF install

---
+++ templates/archlinux-64-aif-experimental/definition.rb
@@ -0,0 +1,28 @@
+Veewee::Session.declare({
+  :cpu_count => '1', :memory_size=> '256', 
+  :disk_size => '10140', :disk_format => 'VDI',:hostiocache => 'off',
+  :os_type_id => 'ArchLinux_64',
+  :iso_file => "archlinux-2010.05-core-x86_64.iso",
+  :iso_src => "http://archlinux.mirror.kangaroot.net/iso/2010.05/archlinux-2010.05-core-x86_64.iso",
+  :iso_md5 => "9e9057702af5826a3b924233bf44fe66",
+  :iso_download_timeout => "1000",
+  :boot_wait => "5", :boot_cmd_sequence => [
+    '<Enter>',
+    '<Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait>',
+    '<Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait>',
+    '<Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait>',
+    'root<Enter>',
+    'dhcpcd eth0<Enter><Wait><Wait>',
+    'echo "sshd: ALL" > /etc/hosts.allow<Enter>',
+    'passwd<Enter>',
+    'vagrant<Enter>',
+    'vagrant<Enter>',
+    '/etc/rc.d/sshd start<Enter><Wait>',
+  ],
+  :kickstart_port => "7122", :kickstart_timeout => "10000", :kickstart_file => "",
+  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :sudo_cmd => "sh '%f'",
+  :shutdown_cmd => "shutdown -H",
+  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => "10000"
+})
---
+++ templates/archlinux-64-aif-experimental/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Now we can run the sequence for installing arch"
+echo "f.i. do an aif install based from templates"
+echo "Dave Simmons will help you :)"
