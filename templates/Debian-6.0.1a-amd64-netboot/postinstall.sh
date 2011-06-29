c2f350c808c75334e2d34823d441f637de2466e1
Marius Ducea <marius.ducea@gmail.com>
2011-06-29 10:43:59 -0700

    let's use the empty 'trick' to have smaller debian boxes also

--- templates/Debian-6.0.1a-amd64-netboot/postinstall.sh
+++ templates/Debian-6.0.1a-amd64-netboot/postinstall.sh
@@ -46,4 +46,9 @@ apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
 
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+# Zero out the free space to save space in the final image:
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
 exit
