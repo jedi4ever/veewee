4fbc24d56975117cbffc7381ffa51969311e25c4
Dave Simons <dave@inuits.be>
2011-07-12 09:17:11 +0200

    some work on arch64 install through AIF
    Signed-off-by: Dave Simons <dave@inuits.be>

--- templates/archlinux-64-aif-experimental/postinstall.sh
+++ templates/archlinux-64-aif-experimental/postinstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo ""
-echo "Now we can run the sequence for installing arch"
-echo "f.i. do an aif install based from templates"
-echo "Dave Simons will help you :)"
+su -c 'aif -p automatic -c aif.cfg'
