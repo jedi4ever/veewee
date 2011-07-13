9231abc666b531a53af973098e3da82e9480a994
Dave Simons <dave@inuits.be>
2011-07-13 02:20:34 +0200

    setup users and ssh access, comment out aif install and try from definition.rb
    Signed-off-by: Dave Simons <dave@inuits.be>

--- templates/archlinux-64-aif-experimental/postinstall.sh
+++ templates/archlinux-64-aif-experimental/postinstall.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-su -c 'aif -p automatic -c aif.cfg'
+# launch automated install
+#su -c 'aif -p automatic -c aif.cfg'
+
+# chroot into the new system
+mount -o bind /dev /mnt/dev
+mount -o bind /sys /mnt/sys
+mount -t proc none /mnt/proc
+chroot /mnt
+
+# make sure ssh is allowed
+cat <<EOF > /etc/hosts.allow
+sshd:	ALL
+EOF
+
+# make sure sshd starts
+sed -i '$s/network /network sshd /' /etc/rc.conf
+
+# set up user accounts
+sed -i 's/root::/root:$1$9mqoT8YL$6pA27gnKGt0P1lQtlRDDb\/:/' /mnt/etc/shadow
+echo 'vagrant:$1$9mqoT8YL$6pA27gnKGt0P1lQtlRDDb/:15167:0:99999:7:::' >> /mnt/etc/shadow
