6359f12fc4c3653614cc5df74ce0121b2e659501
Dave Simons <dave@inuits.be>
2011-07-15 00:30:46 +0200

    remove releng iso references - not needed set timeout before typing back to 5 set ssh port  back to 7222
    Signed-off-by: Dave Simons <dave@inuits.be>

--- templates/archlinux-64-aif-experimental/definition.rb
+++ templates/archlinux-64-aif-experimental/definition.rb
@@ -2,14 +2,11 @@ Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '256', 
   :disk_size => '10140', :disk_format => 'VDI',:hostiocache => 'off',
   :os_type_id => 'ArchLinux_64',
-#  :iso_file => "archlinux-2011.06.10-core-x86_64.iso",
-#  :iso_src => "http://releng.archlinux.org/isos/2011.06.10/archlinux-2011.06.10-core-x86_64.iso",
-#  :iso_md5 => "8e38f5380a08aca2aadae6a8920fa1ee",
   :iso_file => "archlinux-2010.05-core-x86_64.iso",
   :iso_src => "http://archlinux.mirror.kangaroot.net/iso/2010.05/archlinux-2010.05-core-x86_64.iso",
   :iso_md5 => "9e9057702af5826a3b924233bf44fe66",
   :iso_download_timeout => "1000",
-  :boot_wait => "6", :boot_cmd_sequence => [
+  :boot_wait => "5", :boot_cmd_sequence => [
     '<Enter>',
     '<Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait>',
     '<Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait><Wait>',
@@ -25,7 +22,7 @@ Veewee::Session.declare({
   ],
   :kickstart_port => "7122", :kickstart_timeout => "10000", :kickstart_file => "aif.cfg",
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
-  :ssh_host_port => "7224", :ssh_guest_port => "22",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "sh '%f'",
   :shutdown_cmd => "shutdown -h now",
   :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => "10000"
