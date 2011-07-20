787bd1749e284a58cf04c86a90509eea6f709aa1
Dave Simons <dave@inuits.be>
2011-07-20 14:51:18 +0200

    experimental archlinux netboot stuff
    Signed-off-by: Dave Simons <dave@inuits.be>

---
+++ templates/archlinux-x86_64-netboot/postinstall.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# launch automated install
+su -c 'aif -p automatic -c aif.cfg'
+
+# copy over the vbox version file
+/bin/cp -f /root/.vbox_version /mnt/root/.vbox_version
+VBOX_VERSION=$(cat /root/.vbox_version)
+
+# chroot into the new system
+mount -o bind /dev /mnt/dev
+mount -o bind /sys /mnt/sys
+mount -t proc none /mnt/proc
+chroot /mnt <<ENDCHROOT
+
+# make sure network is up and a nameserver is available
+dhcpcd eth0
+
+# sudo setup
+# note: do not use tabs here, it autocompletes and borks the sudoers file
+cat <<EOF > /etc/sudoers
+root    ALL=(ALL)    ALL
+%wheel    ALL=(ALL)    NOPASSWD: ALL
+EOF
+
+# set up user accounts
+passwd<<EOF
+vagrant
+vagrant
+EOF
+useradd -m -G wheel -r vagrant
+passwd -d vagrant
+passwd vagrant<<EOF
+vagrant
+vagrant
+EOF
+
+# make sure ssh is allowed
+echo "sshd:	ALL" > /etc/hosts.allow
+
+# and everything else isn't
+echo "ALL:	ALL" > /etc/hosts.deny
+
+# make sure sshd starts
+sed -i 's:^DAEMONS\(.*\))$:DAEMONS\1 sshd):' /etc/rc.conf
+
+# install mitchellh's ssh key
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# choose a mirror
+sed -i 's/^#\(.*leaseweb.*\)/\1/' /etc/pacman.d/mirrorlist
+
+# update pacman
+pacman -Syy
+pacman -S --noconfirm pacman
+
+# upgrade pacman db
+pacman-db-upgrade
+pacman -Syy
+
+# install some packages
+gem install --no-ri --no-rdoc chef facter
+cd /tmp
+git clone https://github.com/puppetlabs/puppet.git
+cd puppet
+ruby install.rb --bindir=/usr/bin --sbindir=/sbin
+
+# install virtualbox guest additions
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/"$VBOX_VERSION"/VBoxGuestAdditions_"$VBOX_VERSION".iso
+mount -o loop VBoxGuestAdditions_"$VBOX_VERSION".iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_"$VBOX_VERSION".iso
+
+# clean out pacman cache
+pacman -Scc<<EOF
+y
+y
+EOF
+
+# zero out the fs
+dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
+
+ENDCHROOT
+
+# and reboot!
+reboot
