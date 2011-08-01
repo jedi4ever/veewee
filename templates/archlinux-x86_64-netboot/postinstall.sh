e5a660ca82b971f0d002cfcb861c0a02b34665eb
Dave Simons <dave@inuits.be>
2011-08-01 13:34:06 +0200

    dhcp fix in rc.local
    Signed-off-by: Dave Simons <dave@inuits.be>

--- templates/archlinux-x86_64-netboot/postinstall.sh
+++ templates/archlinux-x86_64-netboot/postinstall.sh
@@ -77,6 +77,14 @@ sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm VBoxGuestAdditions_"$VBOX_VERSION".iso
 
+# host-only networking
+cat >> /etc/rc.local <<EOF
+# enable DHCP at boot on eth0
+# See https://wiki.archlinux.org/index.php/Network#DHCP_fails_at_boot
+dhcpcd -k eth0
+dhcpcd -nd eth0
+EOF
+
 # clean out pacman cache
 pacman -Scc<<EOF
 y
