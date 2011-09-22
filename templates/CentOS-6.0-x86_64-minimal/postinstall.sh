ca27c8feedf691d9a0dfd7a7000bdc1a24359912
Brett Porter <brett@maestrodev.com>
2011-09-22 22:38:26 +1000

    add a template for a minimal CentOS install

---
+++ templates/CentOS-6.0-x86_64-minimal/postinstall.sh
@@ -0,0 +1,45 @@
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+
+date > /etc/vagrant_box_build_time
+
+yum -y install gcc make gcc-c++ ruby kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl
+
+cat > /etc/yum.repos.d/puppetlabs.repo << EOM
+[puppetlabs]
+name=puppetlabs
+baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install puppet facter ruby-devel rubygems
+yum -y clean all
+rm /etc/yum.repos.d/{puppetlabs,epel}.repo
+
+gem install --no-ri --no-rdoc chef
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+curl -L -o authorized_keys http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
+chown -R vagrant /home/vagrant/.ssh
+
+# Installing the virtualbox guest additions
+mount /dev/sr0 /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
+
+exit
