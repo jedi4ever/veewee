861a747e0393ef0498e21a7db4073aa5ea9abd00
Patrick Debois <Patrick.Debois@jedi.be>
2011-09-27 08:43:08 +0200

    fixed double include of version

--- lib/veewee/command/version.rb
+++ lib/veewee/command/version.rb
@@ -3,8 +3,11 @@ module Veewee
     class VersionCommand < Base
 
       register "version", "Prints the Veewee version information"
-
+      class_option :includes, :type => :array, :default => ["aa"], :aliases => "-i"
+      class_option :excludes, :type => :array, :default => ["aa"], :aliases => "-e"
       def execute
+      require 'pp'
+        pp options
         env.ui.info "Version : #{Veewee::VERSION} - use at your own risk"
       end
 
