30b3a0d4d5347b90806d1c74cf7597b126097190
Patrick Debois <Patrick.Debois@jedi.be>
2011-10-05 08:28:34 +0200

    provided a second options for info logger

--- lib/veewee/ui.rb
+++ lib/veewee/ui.rb
@@ -11,7 +11,7 @@ module Veewee
     end
 
     [:warn, :error, :info, :confirm].each do |method|
-      define_method(method) do |message|
+      define_method(method,opts=nil) do |message|
         # Log normal console messages
         env.logger.info("ui") { message }
       end
@@ -19,7 +19,7 @@ module Veewee
 
     [:clear_line, :report_progress, :ask, :no?, :yes?].each do |method|
       # By default do nothing, these aren't logged
-      define_method(method) { |*args| }
+      define_method(method, opts=nil) { |*args| }
     end
 
     # A shell UI, which uses a `Thor::Shell` object to talk with
