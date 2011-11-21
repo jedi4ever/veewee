4d454dcb57a48e283ddb500a73c6b5acb27d75c0
Ian Chilton <ian@ichilton.co.uk>
2011-11-21 16:06:14 +0000

    Upgrade to latest ruby + gems on Ubuntu 11.10 templates

--- templates/ubuntu-11.10-server-i386/postinstall.sh
+++ templates/ubuntu-11.10-server-i386/postinstall.sh
@@ -7,7 +7,7 @@ date > /etc/vagrant_box_build_time
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get clean
 
 # Setup sudo to allow no-password sudo for "admin"
@@ -20,24 +20,21 @@ apt-get -y install nfs-common
 
 # Install Ruby from source in /opt so that users of Vagrant
 # can install their own Rubies using packages or however.
-# We must install the 1.8.x series since Puppet doesn't support
-# Ruby 1.9 yet.
-wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p334.tar.gz
-tar xvzf ruby-1.8.7-p334.tar.gz
-cd ruby-1.8.7-p334
+tar xvzf ruby-1.9.2-p290.tar.gz
+cd ruby-1.9.2-p290
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.8.7-p334*
+rm -rf ruby-1.9.2-p290
 
 # Install RubyGems 1.7.2
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.7.2.tgz
-tar xzf rubygems-1.7.2.tgz
-cd rubygems-1.7.2
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.11.tgz
+tar xzf rubygems-1.8.11.tgz
+cd rubygems-1.8.11
 /opt/ruby/bin/ruby setup.rb
 cd ..
-rm -rf rubygems-1.7.2*
+rm -rf rubygems-1.8.11
 
 # Installing chef & Puppet
 /opt/ruby/bin/gem install chef --no-ri --no-rdoc
