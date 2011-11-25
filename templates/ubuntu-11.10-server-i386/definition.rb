d299482b32bbc5ba00169214bf12a9d640c54148
Patrick Debois <Patrick.Debois@jedi.be>
2011-11-25 11:07:36 +0100

    fixed reformatting merge problems

--- templates/ubuntu-11.10-server-i386/definition.rb
+++ templates/ubuntu-11.10-server-i386/definition.rb
@@ -1,25 +1,16 @@
 Veewee::Session.declare({
-<<<<<<< HEAD
-  :cpu_count => '1', :memory_size=> '384', 
-  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
-=======
   :cpu_count => '1', 
   :memory_size=> '384', 
   :disk_size => '10140', 
   :disk_format => 'VDI', 
   :hostiocache => 'off',
->>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
   :os_type_id => 'Ubuntu',
   :iso_file => "ubuntu-11.10-server-i386.iso",
   :iso_src => "http://releases.ubuntu.com/11.10/ubuntu-11.10-server-i386.iso",
   :iso_md5 => "881d188cb1ca5fb18e3d9132275dceda",
   :iso_download_timeout => "1000",
-<<<<<<< HEAD
-  :boot_wait => "10", :boot_cmd_sequence => [
-=======
   :boot_wait => "10", 
   :boot_cmd_sequence => [
->>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
     '<Esc><Esc><Enter>',
     '/install/vmlinuz noapic preseed/url=http://%IP%:%PORT%/preseed.cfg ',
     'debian-installer=en_US auto locale=en_US kbd-chooser/method=us ',
@@ -28,14 +19,6 @@ Veewee::Session.declare({
     'keyboard-configuration/layout=USA keyboard-configuration/variant=USA console-setup/ask_detect=false ',
     'initrd=/install/initrd.gz -- <Enter>'
   ],
-<<<<<<< HEAD
-  :kickstart_port => "7122", :kickstart_timeout => "10000", :kickstart_file => "preseed.cfg",
-  :ssh_login_timeout => "10000", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
-  :ssh_host_port => "7222", :ssh_guest_port => "22",
-  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
-  :shutdown_cmd => "shutdown -P now",
-  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => "10000"
-=======
   :kickstart_port => "7122", 
   :kickstart_timeout => "10000", 
   :kickstart_file => "preseed.cfg",
@@ -49,5 +32,4 @@ Veewee::Session.declare({
   :shutdown_cmd => "shutdown -P now",
   :postinstall_files => [ "postinstall.sh"], 
   :postinstall_timeout => "10000"
->>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
 })
