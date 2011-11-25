121c1b81b06ef049936d38f9a76cb39c1afbd4bd
Patrick Debois <Patrick.Debois@jedi.be>
2011-11-25 11:06:07 +0100

    fixed reformatting merge problems

--- templates/Debian-6.0.3-amd64-netboot/postinstall.sh
+++ templates/Debian-6.0.3-amd64-netboot/postinstall.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #http://adrianbravo.tumblr.com/post/644860401
 
 date > /etc/vagrant_box_build_time
@@ -33,10 +34,57 @@ chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 #the netboot install the virtualbox stuff so we have to remove it
+=======
+date > /etc/vagrant_box_build_time
+
+# Update the box
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
+apt-get -y install curl unzip
+apt-get clean
+
+# Set up sudo
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+# Install Ruby from packages
+apt-get -y install ruby ruby-dev libruby1.8 ri
+
+# Install Rubygems from source
+rg_ver=1.6.2
+curl -o /tmp/rubygems-${rg_ver}.zip \
+  "http://production.cf.rubygems.org/rubygems/rubygems-${rg_ver}.zip"
+(cd /tmp && unzip rubygems-${rg_ver}.zip && \
+  cd rubygems-${rg_ver} && ruby setup.rb --no-format-executable)
+rm -rf /tmp/rubygems-${rg_ver} /tmp/rubygems-${rg_ver}.zip
+
+# Install Chef & Puppet
+gem install chef --no-ri --no-rdoc
+gem install puppet --no-ri --no-rdoc
+
+# Install vagrant keys
+mkdir -p /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+curl -Lo /home/vagrant/.ssh/authorized_keys \
+  'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Customize the message of the day
+echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
+
+# The netboot installs the VirtualBox support (old) so we have to remove it
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
 /etc/init.d/virtualbox-ose-guest-utils stop
 rmmod vboxguest
 aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
 
+<<<<<<< HEAD
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
@@ -49,6 +97,21 @@ apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
 
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
+=======
+# Install the VirtualBox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+curl -Lo /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso \
+  "http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso"
+mount -o loop /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+# Clean up
+apt-get -y remove linux-headers-$(uname -r) build-essential
+apt-get -y autoremove
+
+rm /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso 
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
 
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
@@ -59,7 +122,10 @@ echo "cleaning up dhcp leases"
 rm /var/lib/dhcp3/*
 
 # Make sure Udev doesn't block our network
+<<<<<<< HEAD
 # http://6.ptmc.org/?p=164
+=======
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
 echo "cleaning up udev rules"
 rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
--- templates/ubuntu-11.10-server-amd64/definition.rb
+++ templates/ubuntu-11.10-server-amd64/definition.rb
@@ -1,12 +1,25 @@
 Veewee::Session.declare({
+<<<<<<< HEAD
   :cpu_count => '1', :memory_size=> '384', 
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
+=======
+  :cpu_count => '1', 
+  :memory_size=> '384', 
+  :disk_size => '10140', 
+  :disk_format => 'VDI', 
+  :hostiocache => 'off',
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
   :os_type_id => 'Ubuntu_64',
   :iso_file => "ubuntu-11.10-server-amd64.iso",
   :iso_src => "http://releases.ubuntu.com/11.10/ubuntu-11.10-server-amd64.iso",
   :iso_md5 => "f8a0112b7cb5dcd6d564dbe59f18c35f",
   :iso_download_timeout => "1000",
+<<<<<<< HEAD
   :boot_wait => "10", :boot_cmd_sequence => [
+=======
+  :boot_wait => "10", 
+  :boot_cmd_sequence => [
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
     '<Esc><Esc><Enter>',
     '/install/vmlinuz noapic preseed/url=http://%IP%:%PORT%/preseed.cfg ',
     'debian-installer=en_US auto locale=en_US kbd-chooser/method=us ',
@@ -15,10 +28,26 @@ Veewee::Session.declare({
     'keyboard-configuration/layout=USA keyboard-configuration/variant=USA console-setup/ask_detect=false ',
     'initrd=/install/initrd.gz -- <Enter>'
   ],
+<<<<<<< HEAD
   :kickstart_port => "7122", :kickstart_timeout => "10000", :kickstart_file => "preseed.cfg",
   :ssh_login_timeout => "10000", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
   :shutdown_cmd => "shutdown -P now",
   :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => "10000"
+=======
+  :kickstart_port => "7122", 
+  :kickstart_timeout => "10000", 
+  :kickstart_file => "preseed.cfg",
+  :ssh_login_timeout => "10000", 
+  :ssh_user => "vagrant", 
+  :ssh_password => "vagrant", 
+  :ssh_key => "",
+  :ssh_host_port => "7222", 
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "shutdown -P now",
+  :postinstall_files => [ "postinstall.sh"], 
+  :postinstall_timeout => "10000"
+>>>>>>> 34932ec8fc8d87efb3846e336400a726f381252e
 })
