86c182581ddc2be5fbff5b92278dbe298f73a3c4
Chris McClimans <chris@hippiehacker.org>
2012-01-05 17:45:33 +1300

    updated win7entx86

--- templates/windows-7-enterprise-i386/definition.rb
+++ templates/windows-7-enterprise-i386/definition.rb
@@ -7,9 +7,9 @@ Veewee::Session.declare({
     # The 90-day Trial is offered for a limited time and in limited quantity.
     # The download will be available through June 30th, 2012, while supplies last.
     :iso_file => "7600.16385.090713-1255_x86fre_enterprise_en-us_EVAL_Eval_Enterprise-GRMCENEVAL_EN_DVD.iso",
-    :iso_src => "http://wb.dlservice.microsoft.com/dl/download/release/Win7/3/b/a/3bac7d87-8ad2-4b7a-87b3-def36aee35fa/7600.16385.090713-1255_x86fre_enterprise_en-us_EVAL_Eval_Enterprise-GRMCENEVAL_EN_DVD.iso?lcid=1033",
+    :iso_src => "http://wb.dlservice.microsoft.com/dl/download/release/Win7/3/b/a/3bac7d87-8ad2-4b7a-87b3-def36aee35fa/7600.16385.090713-1255_x86fre_enterprise_en-us_EVAL_Eval_Enterprise-GRMCENEVAL_EN_DVD.iso",
     :iso_md5 => "62675A3B76D21815367F372961B71A56",
-    :iso_download_timeout => "10000",
+    :iso_download_timeout => "100000",
 
     :cpu_count => '1',
     :memory_size=> '512', 
@@ -19,20 +19,16 @@ Veewee::Session.declare({
       "Autounattend.xml",
       "install-winrm.bat",
       "install-cygwin-sshd.bat",
-      "cygwin-setup.exe",
       "oracle-cert.cer"
     ],
 
-    :boot_wait => "660", #ten minutes, ten seconds
-    :boot_cmd_sequence => [ 
-      '<Tab><Spacebar><Tab><Tab><Tab><Spacebar>'
-    ],
-
+    :boot_wait => "1", #ten minutes, ten seconds
+    :boot_cmd_sequence => [''],
 
     :ssh_login_timeout => "10000",
     # Actively attempt to winrm (no ssh on base windows) in for 10000 seconds
     :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "", 
-    :ssh_host_port => "59857", :ssh_guest_port => "22",
+    :ssh_host_port => "59867", :ssh_guest_port => "22",
     # And run postinstall.sh for up to 10000 seconds
     :postinstall_timeout => "10000",
     :postinstall_files => ["postinstall.sh"],
@@ -40,5 +36,5 @@ Veewee::Session.declare({
     :sudo_cmd => "sh '%f'",
     # Shutdown is different as well
     #:shutdown_cmd => "shutdown /s /t 0 /d P:4:1 /c \"Vagrant Shutdown\"",
-    :shutdown_cmd => "shutdown /p /t 60 /c \"Vagrant Shutdown\" /f /d p:4:1",
+    :shutdown_cmd => "shutdown /p /t 0 /c \"Vagrant Shutdown\" /f /d p:4:1",
   })
--- templates/windows-7-enterprise-i386/postinstall.sh
+++ templates/windows-7-enterprise-i386/postinstall.sh
@@ -20,15 +20,15 @@ chmod +x apt-cyg
 mv apt-cyg /usr/local/bin/
 
 # 7zip will allow us to extract a file from an ISO
-wget http://downloads.sourceforge.net/sevenzip/7z920-x64.msi
-msiexec /qb /i 7z920-x64.msi
+wget http://downloads.sourceforge.net/sevenzip/7z922.msi
+msiexec /qb /i 7z922.msi
 
 # Download Virtualbox Additions
 VBOX_VERSION="4.1.8" #"4.0.8"
 wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 
 # Extract the installer from the ISO (WHY WHY WHY isn't this available not bundled within an ISO)
-/cygdrive/c/Program\ Files/7-Zip/7z.exe x VBoxGuestAdditions_$VBOX_VERSION.iso VBoxWindowsAdditions-amd64.exe
+/cygdrive/c/Program\ Files/7-Zip/7z.exe x VBoxGuestAdditions_$VBOX_VERSION.iso VBoxWindowsAdditions-x86.exe
 
 # Mark Oracle as a trusted installer
 #http://blogs.msdn.com/b/steverac/archive/2009/07/09/adding-certificates-to-the-local-certificates-store-and-setting-local-policy-using-a-command-line-system-center-updates-publisher-example.aspx
@@ -36,7 +36,7 @@ wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_
 certutil -addstore -f "TrustedPublisher" a:oracle-cert.cer
 
 # Install the Virtualbox Additions
-./VBoxWindowsAdditions-amd64.exe /S
+./VBoxWindowsAdditions-x86.exe /S
 
 
 #Rather than do the manual install of ruby and chef, just use the opscode msi
@@ -70,5 +70,5 @@ net.exe use  '\\vboxsvr\veewee-validation'
 
 # Reboot
 # http://www.techrepublic.com/blog/datacenter/restart-windows-server-2003-from-the-command-line/245
-shutdown.exe /r /t 0 /d p:2:4 /c "Vagrant initial reboot"
+shutdown.exe /s /t 0 /d p:2:4 /c "Vagrant initial reboot"
 
