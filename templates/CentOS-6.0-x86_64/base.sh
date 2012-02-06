4cf9b729a24feb2389b5587b9411aaee3e467d76
Tom De Vylder <tom@penumbra.be>
2012-02-06 14:32:36 +0100

    Split postinstall for centos-6.0-x86_64

---
+++ templates/CentOS-6.0-x86_64/base.sh
@@ -0,0 +1,12 @@
+# Base install
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
