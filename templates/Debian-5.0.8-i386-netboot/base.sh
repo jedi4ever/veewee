ad861622c4c0d710b169aab2c19ca0d8c855643d
Tom De Vylder <tom@penumbra.be>
2012-02-06 14:02:41 +0100

    initial split template for debian 5.0.8 i386

---
+++ templates/Debian-5.0.8-i386-netboot/base.sh
@@ -0,0 +1,9 @@
+#Updating the box
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
+apt-get clean
+
+#Setting up sudo
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/vagrant ALL=(ALL) ALL/vagrant ALL=NOPASSWD:ALL/g' /etc/sudoers
