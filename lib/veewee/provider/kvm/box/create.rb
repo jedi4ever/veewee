ac3def19a356c00f37c4903949649edba1766715
Patrick Debois <Patrick.Debois@jedi.be>
2012-02-12 12:54:23 +0100

    suppport for kvm floppy

--- lib/veewee/provider/kvm/box/create.rb
+++ lib/veewee/provider/kvm/box/create.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Veewee
   module Provider
     module Kvm
@@ -6,8 +8,14 @@ module Veewee
         def create(options={})
           # Assemble the Virtualmachine and set all the memory and other stuff"
 
-          #memory_size,cpu_count, volume_size
 
+          create_server(options)
+          create_volume(options)
+          self.create_floppy("virtualfloppy.img")
+        end
+
+        def create_server(options)
+          #memory_size,cpu_count, volume_size
           s=@connection.servers.create(
             :name => name,
             :memory_size => definition.memory_size.to_i*1024,
@@ -21,8 +29,45 @@ module Veewee
         end
 
         # Create the volume of a new vm
-        def create_volume
+        def create_volume(options)
           # Creating the volume is part of the server creation
+        end
+
+        def add_floppy
+          # Get a raw libvirt connection
+          c=@connection.raw
+
+          # Retrieve the domain
+          domain=c.lookup_domain_by_name(name)
+
+          # Retrieve the existing XML from the domain
+          domain_xml=domain.xml_desc
+
+          # Convert the xml nokogiri doc
+          domain_doc=Nokogiri::XML(domain_xml)
+
+          # Find the device section
+          devices=domain_doc.xpath('/domain/devices').first
+          # The floppy xml representation
+          floppy_xml="<disk type='file' device='floppy'><driver name='qemu' type='raw'/><source file='"+
+          File.join(definitition.path,"virtualfloppy.vfd") +
+          "'/><target dev='fda' bus='fdc'/><address type='drive' controller='0' bus='0' unit='0'/></disk>
+          <controller type='fdc' index='0'>"
+
+          # Convert the floppy xml to nokogiri
+          floppy_doc=Nokogiri::XML(floppy_xml)
+
+          # Add the floppy to the devices section
+          devices.add_child(floppy_doc.root)
+
+          # Get the raw xml of the changed document
+          new_xml=domain_doc.to_xml
+
+          # Undefine the existing domain 
+          s.undefine
+
+          # Re-define the domain
+          c.define_domain_xml(new_xml)
         end
 
       end #End Module
