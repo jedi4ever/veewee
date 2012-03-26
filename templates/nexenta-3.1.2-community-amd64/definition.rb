584fa4cd95a8d36146bfc85771d825a690f9fcc4
Lee Thompson <stagr.lee@gmail.com>
2012-03-26 01:02:02 -0500

    some fixes, experiment won't work due to license keys can't be pregenerated

--- templates/nexenta-3.1.2-community-amd64/definition.rb
+++ templates/nexenta-3.1.2-community-amd64/definition.rb
@@ -2,8 +2,7 @@
 # the iso directory and rerun.  This script essentially makes a Nexenta
 # appliance a general purpose system Nexenta which is counter to the goals of
 # NexentaStor...  But, hackers will hack... its the way of things.
-
-Veewee::Definition.declare({
+Veewee::Session.declare({
   :cpu_count => '2',
   :memory_size=> '1024',
   :disk_size => '10000',
@@ -16,33 +15,34 @@ Veewee::Definition.declare({
   :iso_download_instructions => "You'll need to manually unzip the ISO file\n",
   :iso_md5 => "260270ba7162298c6f597eed78009079",
   :iso_download_timeout => 1000,
-  :boot_wait => "10",
+  :boot_wait => "15",
   :boot_cmd_sequence => [ 
-    '<Enter>',                # Install NexentaStor Community Edition
-    '<Wait>'*5'<Enter>',      # I agree
-    '<Enter>',                # Okay
-    '<Wait>'*5'<Enter>',      # Select Disk Found
-    '<Enter>',                # Yes, kill the disk
-    '<Wait>'*400'<Enter>',    # Main install and acknowledge a reboot
-    '<Wait>'*200'<Enter>',    # Agree to license
-    '<Wait>'*30'XXXX<Enter>', # Replace XXXX with your license!!!
-    'n<Enter>',               # don't reconfig
-    '<Enter>',                # talk http
-    '<Enter>',                # port 2000 is fine
-    '<Wait>'*400,             # Long config part
-    'root<Enter>',            # root login
-    'nexenta<Enter>',         # nexenta is the password
-    'option expert_mode = 1<Enter>', # unlocking the ability to go to bash
-    '!bash<Enter>y',           # you are now at the shell
-    'useradd -g vagrant -G rvm -c "Added by VeeWee" -b /var  -m vagrant<Enter>',
-    'apt-get install sudo<Enter>',
-    'apt-get update<Enter>',
-    'echo "vagrant ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers<Enter>',
-    'apt-get install gcc -o APT::Install-Suggests=true<Enter>',
-    'apt-get install ca-certificates<Enter>',
-    'passwd vagrant<Enter>',  # set the vagrant passwd
-    'vagrant<Enter>',
-    'vagrant<Enter>'          # Should be good to postinstall now
+    '<Enter>',                    # Install NexentaStor Community Edition
+    '<Wait>'*55,'<Enter>',        # I agree
+    '<Wait>'*7,'<Enter>',         # Okay
+    '<Wait>'*25,'<Enter>',        # Select Disk Found
+    '<Wait>'*3,'<Enter>',         # Yes, kill the disk
+    '<Wait>'*660,'<Enter>',       # Main install and acknowledge a reboot
+    '<Wait>'*100,'<Enter>',       # Agree to license
+    '<Wait>'*30,'XXXX<Enter>',    # Replace XXXX with your license!!!
+    '<Wait><Wait>n<Enter>',       # don't reconfig
+    '<Wait><Wait><Enter>',        # talk http
+    '<Wait><Wait><Enter>',        # port 2000 is fine
+    '<Wait>'*600,                 # Long config part
+    '<Wait><Wait>root<Enter>',    # root login
+    '<Wait><Wait>nexenta<Enter>', # nexenta is the password
+    '<Wait><Wait>option expert_mode = 1<Enter>', # unlocking the ability to go to bash
+    '<Wait><Wait>!bash<Enter>y',           # you are now at the shell
+    '<Wait><Wait>useradd -g vagrant -G rvm -c "Added by VeeWee" -b /var  -m vagrant<Enter>',
+    '<Wait><Wait>apt-get install sudo<Enter>',
+    '<Wait><Wait>apt-get update<Enter>',
+    '<Wait><Wait>echo "vagrant ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers<Enter>',
+    '<Wait><Wait>apt-get install gcc -o APT::Install-Suggests=true<Enter>',
+    '<Wait><Wait>apt-get install ca-certificates<Enter>',
+    '<Wait><Wait>passwd vagrant<Enter>',  # set the vagrant passwd
+    '<Wait><Wait>vagrant<Enter>',
+    '<Wait><Wait>vagrant<Enter>',         # Should be good to postinstall now
+    '<Wait><Wait>'
     ],
   :ssh_login_timeout => "10000",
   :ssh_user => "vagrant",
