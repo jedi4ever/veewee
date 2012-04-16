07e2b176e78b6de8cc060d27273ebd9a0bd14c6b
Nick Stielau <nick.stielau@gmail.com>
2012-04-15 23:04:25 -0700

    Setting the correct OS type for Fedora 16 64bit definition.

--- templates/Fedora-16-x86_64/definition.rb
+++ templates/Fedora-16-x86_64/definition.rb
@@ -2,7 +2,7 @@ Veewee::Session.declare({
   # Minimum RAM requirement for installation is 768MB.
   :cpu_count => '1', :memory_size=> '768',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off', :hwvirtext => 'on',
-  :os_type_id => 'Fedora',
+  :os_type_id => 'Fedora_64',
   :iso_file => "Fedora-16-x86_64-DVD.iso",
   :iso_src => "http://download.fedoraproject.org/pub/fedora/linux/releases/16/Fedora/x86_64/iso/Fedora-16-x86_64-DVD.iso",
   :iso_md5 => "bb38ea1fe4b2fc69e7a6e15cf1c69c91",
