2de2f26880a3661a0db505c297ab2558b48cb495
Marco Silva <marcodasilva@gmail.com>
2012-04-19 10:39:01 +0200

    Updated Rubygem version
    Updated Rubygem version from 1.6.2 to 1.8.22. With previous version,
    installation would fail.

--- templates/Debian-6.0.4-i386-netboot/ruby.sh
+++ templates/Debian-6.0.4-i386-netboot/ruby.sh
@@ -2,7 +2,7 @@
 apt-get -y install ruby ruby-dev libopenssl-ruby1.8 irb ri rdoc
 
 # Install Rubygems from source
-rg_ver=1.6.2
+rg_ver=1.8.22
 curl -o /tmp/rubygems-${rg_ver}.zip \
   "http://production.cf.rubygems.org/rubygems/rubygems-${rg_ver}.zip"
 (cd /tmp && unzip rubygems-${rg_ver}.zip && \
