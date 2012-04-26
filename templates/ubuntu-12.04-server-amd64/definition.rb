48d23351586010ccfc773942e524e1b8db5889f1
skim <skim.la@gmail.com>
2012-04-26 13:13:37 -0500

    updates to ruby-1.9.2-p320, rubygems 1.8.23

--- templates/ubuntu-12.04-server-amd64/definition.rb
+++ templates/ubuntu-12.04-server-amd64/definition.rb
@@ -7,7 +7,7 @@ Veewee::Session.declare({
   :os_type_id => 'Ubuntu_64',
   :iso_file => "ubuntu-12.04-server-amd64.iso",
   :iso_src => "http://releases.ubuntu.com/12.04/ubuntu-12.04-server-amd64.iso",
-  :iso_md5 => "f2e921788d35bbdf0336d05d228136eb"
+  :iso_md5 => "f2e921788d35bbdf0336d05d228136eb",
   :iso_download_timeout => "1000",
   :boot_wait => "4",
   :boot_cmd_sequence => [
