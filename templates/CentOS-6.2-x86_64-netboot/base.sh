195ccc52dc3575cf67154ed54fcf63418a1ad193
Mike Fiedler <miketheman@gmail.com>
2012-08-06 15:51:53 -0400

    Updated CentOS-6.2-x86_64-netboot for consistency

--- templates/CentOS-6.2-x86_64-netboot/base.sh
+++ templates/CentOS-6.2-x86_64-netboot/base.sh
@@ -10,4 +10,5 @@ enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget
+
