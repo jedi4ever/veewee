de794e2d100de0ea6715bede78a4c4ddeb4aef7c
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-19 03:38:08 -0500

    Added libyaml-dev.

--- templates/ubuntu-12.10-server-amd64/postinstall.sh
+++ templates/ubuntu-12.10-server-amd64/postinstall.sh
@@ -2,13 +2,12 @@
 
 date > /etc/vagrant_box_build_time
 
-
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim
 apt-get clean
 
