1faa498948ce90dd4e75b8cd8aa0b0d40de191e8
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-19 21:58:16 -0500

    Install ruby-dev package with the rest of the ruby packaged.

--- templates/ubuntu-12.10-server-amd64-packages/postinstall.sh
+++ templates/ubuntu-12.10-server-amd64-packages/postinstall.sh
@@ -7,7 +7,7 @@ date > /etc/vagrant_box_build_time
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev ruby-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim
 apt-get clean
 
@@ -36,7 +36,7 @@ groupadd -r puppet
 apt-get -y install nfs-common
 
 # Install Ruby from packages
-apt-get -y install ruby rubygems
+apt-get -y install ruby rubygems ruby-dev
 
 # Installing chef & Puppet
 gem install chef --no-ri --no-rdoc
