5d36c245a8d217410e26f06e86f8f44d98ad7837
Hippie Hacker <chris@hippiehacker.org>
2012-10-22 23:44:24 -0700

    remove debugging proxy port

--- lib/veewee/provider/virtualbox/box/helper/winrm_options.rb
+++ lib/veewee/provider/virtualbox/box/helper/winrm_options.rb
@@ -15,8 +15,8 @@ module Veewee
           winrm_options={
             :user => definition.winrm_user,
             :pass => definition.winrm_password,
-#            :port => port,
-            :port => (port.to_i+1).to_s, # debug
+            :port => port,
+#            :port => (port.to_i+1).to_s, # debug, by running charles with a reverse proxy
             :basic_auth_only => true,
             :timeout => definition.winrm_login_timeout.to_i,
             :operation_timeout => 600 # ten minutes
