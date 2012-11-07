2f8626377fb80ad9b3d15858e3fde0c7e516bac5
Patrick Debois <Patrick.Debois@jedi.be>
2012-11-07 11:20:06 +0100

    added more puppet and vbox reboot on win7 winrm template

--- templates/windows-7-enterprise-amd64-winrm/install-puppet.bat
+++ templates/windows-7-enterprise-amd64-winrm/install-puppet.bat
@@ -1,2 +1,4 @@
 cmd /C cscript %TEMP%\wget.vbs /url:https://downloads.puppetlabs.com/windows/puppet-3.0.1.msi /path:%TEMP%\puppet.msi
 cmd /C msiexec /qn /i %TEMP%\puppet.msi
+cmd /C gem install sys-admin win32-process win32-dir win32-taskscheduler
+cmd /C gem install win32-service --platform=mswin32
--- templates/windows-7-enterprise-amd64-winrm/install-vbox.bat
+++ templates/windows-7-enterprise-amd64-winrm/install-vbox.bat
@@ -1,4 +1,4 @@
-# with this, we can open the iso, and extract the VBoxWindowsAdditions.exe!
-# http://downloads.sourceforge.net/sevenzip/7z920.exe
 cmd /c certutil -addstore -f "TrustedPublisher" a:oracle-cert.cer
 cmd /c e:\VBoxWindowsAdditions-amd64.exe /S
+
+shutdown /r /t 2 /c "Install Virtualbox Guest Additions Reboot" /f /d p:4:1
