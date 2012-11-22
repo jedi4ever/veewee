564b2fddc54f21d7015036e067f03734d0f35204
Trung Lê <joneslee85@gmail.com>
2012-11-22 11:49:47 +1100

    Bump ruby version to 1.9.3 on all ubuntu 12.04 or newer boxes

--- templates/ubuntu-12.10-server-i386/postinstall.sh
+++ templates/ubuntu-12.10-server-i386/postinstall.sh
@@ -38,24 +38,14 @@ apt-get -y install nfs-common
 
 # Install Ruby from source in /opt so that users of Vagrant
 # can install their own Rubies using packages or however.
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p286.tar.gz
-tar xvzf ruby-1.9.3-p286.tar.gz
-cd ruby-1.9.3-p286
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
+tar xvzf ruby-1.9.3-p327.tar.gz
+cd ruby-1.9.3-p327
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.9.3-p286
-rm ruby-1.9.3-p286.tar.gz
-
-# Install RubyGems 1.8.24
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
-tar xzf rubygems-1.8.24.tgz
-cd rubygems-1.8.24
-/opt/ruby/bin/ruby setup.rb
-cd ..
-rm -rf rubygems-1.8.24
-rm rubygems-1.8.24.tgz
+rm -rf ruby-1.9.3*
 
 # Installing chef & Puppet
 /opt/ruby/bin/gem install chef --no-ri --no-rdoc
