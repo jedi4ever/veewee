aca6dc3ef1936413391a35dde76f13ed9e20d902
Julian C. Dunn <jdunn@secondmarket.com>
2012-12-02 22:17:19 -0500

    Fix spelling error: "succesfully" --> "successfully".

--- lib/veewee/provider/core/box/build.rb
+++ lib/veewee/provider/core/box/build.rb
@@ -82,7 +82,7 @@ module Veewee
 
           self.handle_postinstall(options)
 
-          ui.success "The box #{name} was build succesfully!"
+          ui.success "The box #{name} was build successfully!"
           ui.info "You can now login to the box with:"
           if (definition.winrm_user && definition.winrm_password)
             env.ui.info winrm_command_string
