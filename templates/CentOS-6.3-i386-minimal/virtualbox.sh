365eee96e72f36b6eb08be664e1c4da84f6199f1
Jason Edgecombe <jason@rampaginggeek.com>
2012-12-23 22:41:41 -0500

    CENTOS 6.3: Make the 32bit basebox build
    Rename the veewee user to vagrant.
    Make the entire build work.

--- templates/CentOS-6.3-i386-minimal/virtualbox.sh
+++ templates/CentOS-6.3-i386-minimal/virtualbox.sh
@@ -1,8 +1,8 @@
 # Installing the virtualbox guest additions
-VBOX_VERSION=$(cat /home/veewee/.vbox_version)
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/veewee/VBoxGuestAdditions_*.iso
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
 
