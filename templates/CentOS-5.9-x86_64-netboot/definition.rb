85667cff1f173038cb6c55fe3194ede876caafb7
Ryan Cavicchioni <rcavicchioni@gmail.com>
2013-01-18 16:39:47 -0600

    Accidentally deleted shutdown command.

--- templates/CentOS-5.9-x86_64-netboot/definition.rb
+++ templates/CentOS-5.9-x86_64-netboot/definition.rb
@@ -11,5 +11,6 @@ Veewee::Session.declare({
   :ssh_login_timeout => "10000", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
   :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => 10000
 })
