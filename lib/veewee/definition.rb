9b21fb09114c59130673e9956e658c33cc238f48
manin <mariano.bayo@gmail.com>
2013-02-11 03:51:13 +0100

    Added disk_count definition. Only implemented on VirtualBox

--- lib/veewee/definition.rb
+++ lib/veewee/definition.rb
@@ -11,7 +11,7 @@ module Veewee
     attr_accessor :path
 
     attr_accessor :cpu_count, :memory_size, :video_memory_size, :iso_file
-    attr_accessor :disk_size, :disk_format, :disk_variant
+    attr_accessor :disk_size, :disk_format, :disk_variant, :disk_count
 
     attr_accessor :os_type_id
 
@@ -79,7 +79,7 @@ module Veewee
       @postinstall_files = [] ; @postinstall_timeout = 10000 ;
 
       @iso_file = ""
-      @disk_size = '10240' ; @disk_format = 'VDI' ; @disk_variant = 'Standard'
+      @disk_size = '10240' ; @disk_format = 'VDI' ; @disk_variant = 'Standard' ; @disk_count = 1
       @use_sata = true
 
       #        :hostiocache => 'off' ,
--- veewee.gemspec
+++ veewee.gemspec
@@ -33,6 +33,7 @@ Gem::Specification.new do |s|
   s.add_dependency "childprocess"
   s.add_dependency "grit"
   s.add_dependency "fission", "0.4.0"
+  s.add_dependency "em-winrm"
 
   # Modified dependency version, as libxml-ruby dependency has been removed in version 2.1.1
   # See : https://github.com/ckruse/CFPropertyList/issues/14
