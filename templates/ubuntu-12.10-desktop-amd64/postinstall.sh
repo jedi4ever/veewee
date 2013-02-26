9443f2cc2c3a79ca5ed0d7854d1209d911b3b5bb
Winston Chang <winston@stdout.org>
2013-02-26 15:00:52 -0600

    Remove downloaded packages to save space

--- templates/ubuntu-12.10-desktop-amd64/postinstall.sh
+++ templates/ubuntu-12.10-desktop-amd64/postinstall.sh
@@ -82,6 +82,9 @@ tasksel install ubuntu-desktop
 apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
 
+# Remove downloaded packages
+apt-get clean
+
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
