b360096e775a4b4fb5953ae461e593b93fd1928b
Pete Cheslock <petecheslock@gmail.com>
2013-02-27 23:30:50 -0500

    Update shutdown command for Freebsd 9.x versions

--- templates/freebsd-9.0-RELEASE-amd64/definition.rb
+++ templates/freebsd-9.0-RELEASE-amd64/definition.rb
@@ -26,7 +26,7 @@ Veewee::Definition.declare({
   :ssh_login_timeout => "10000", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "sudo '%f'",
-  :shutdown_cmd => "shutdown -H",
+  :shutdown_cmd => "shutdown -h now",
   :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => "10000"
 })
 #'setkmap=us dodhcp=eth0 dhcphostname=%NAME% ar_source=http://%IP%:%PORT%/ autoruns=0 rootpass=vagrant',
