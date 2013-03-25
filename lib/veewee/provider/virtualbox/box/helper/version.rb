872484bd5d6f24b9f74fac598262a9490cce9806
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2013-03-25 14:32:18 +0000

    Make is_windows a callable function

--- lib/veewee/provider/virtualbox/box/helper/version.rb
+++ lib/veewee/provider/virtualbox/box/helper/version.rb
@@ -4,13 +4,16 @@ module Veewee
       module BoxCommand
         UNSYNCED_VERSIONS = {"4.2.1" => "4.2.0", "4.1.23" => "4.1.22"}
 
+		def is_windows?
+          return (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+		end
+
         # Return the major/minor/incremental version of VirtualBox.
         # For example: 4.1.8_Debianr75467 -> 4.1.8
         def vbox_version
           command="#{@vboxcmd} --version"
           stderr = "/dev/null"
-          is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-          stderr = "nul" if is_windows
+          stderr = "nul" if self.is_windows?
           shell_results=shell_exec("#{command}",{:mute => true, :stderr => stderr})
           version=shell_results.stdout.strip.split(/[^0-9\.]/)[0]
           return version
