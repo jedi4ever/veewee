0169701f9262abc7071b201f8c5fe6def6c23f7a
Michael Dwyer <mdwyer@tamu.edu>
2013-03-27 17:03:20 -0500

    Fixed error in previous commit.

--- lib/veewee/provider/virtualbox/box/helper/natinterface.rb
+++ lib/veewee/provider/virtualbox/box/helper/natinterface.rb
@@ -7,6 +7,7 @@ module Veewee
           command="#{@vboxcmd} showvminfo --details --machinereadable \"#{self.name}\""
           shell_results=shell_exec("#{command}")
 
+          nats=shell_results.stdout.split(/\n/).grep(/^nic/).grep(/nat/)
           if nats.length > 0 then nats[0].split('=')[0][-1,1] end
         end
 
