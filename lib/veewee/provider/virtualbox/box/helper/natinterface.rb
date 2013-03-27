8ad7818c66e748256756ae0672cd69d7b3cfdfa5
Michael Dwyer <mdwyer@tamu.edu>
2013-03-27 15:50:43 -0500

    Fix for #598.
    Skip port-forwarding for VMs that do not have a NAT NIC.

--- lib/veewee/provider/virtualbox/box/helper/natinterface.rb
+++ lib/veewee/provider/virtualbox/box/helper/natinterface.rb
@@ -7,8 +7,7 @@ module Veewee
           command="#{@vboxcmd} showvminfo --details --machinereadable \"#{self.name}\""
           shell_results=shell_exec("#{command}")
 
-          nic_id=shell_results.stdout.split(/\n/).grep(/^nic/).grep(/nat/)[0].split('=')[0][-1,1]
-          return nic_id
+          if nats.length > 0 then nats[0].split('=')[0][-1,1] end
         end
 
       end
