db33a431e5a0b10ca48885fe3ac621f757522835
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-03-29 12:05:07 +0000

    Update scientificlinux baseboxes to 6.4, adopt configs from Centos 6.4 templates

---
+++ templates/scientificlinux-6.4-i386-netboot/definition.rb
@@ -0,0 +1,40 @@
+Veewee::Session.declare({
+  :cpu_count => '1',
+  :memory_size=> '480',
+  :disk_size => '10140',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :os_type_id => 'RedHat',
+  :iso_file => "SL-64-i386-2013-03-18-boot.iso",
+  :iso_src => "http://ftp.heanet.ie/pub/rsync.scientificlinux.org/6.4/i386/iso/SL-64-i386-2013-03-18-boot.iso",
+  :iso_md5 => "97bd4b18c8179164a1a22d8726dd160e",
+  :iso_download_timeout => 1000,
+  :boot_wait => "15",
+  :boot_cmd_sequence => [
+    '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => 10000,
+  :kickstart_file => "ks.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "veewee",
+  :ssh_password => "veewee",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
+  :postinstall_files => [
+    "base.sh",
+    "ruby.sh",
+    "chef.sh",
+    "puppet.sh",
+    "vagrant.sh",
+    "virtualbox.sh",
+    #"kvm.sh",
+    #"vmfusion.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => 10000
+})
