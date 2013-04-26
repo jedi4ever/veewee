90da688fdd4ffe64a0273a093afac5a7867f5d85
Ken Barber <ken@bob.sh>
2013-04-26 18:56:08 +0100

    Cleanup persistent mac addresses from Centos 6.4
    Signed-off-by: Ken Barber <ken@bob.sh>

--- templates/CentOS-6.4-x86_64-minimal/cleanup.sh
+++ templates/CentOS-6.4-x86_64-minimal/cleanup.sh
@@ -3,3 +3,6 @@ yum -y clean all
 rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 
+# Remove traces of mac address from network configuration
+sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+rm /etc/udev/rules.d/70-persistent-net.rules
