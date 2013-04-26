f7a621ed57707ee6d708b20b9e565931485ab4c0
Ken Barber <ken@bob.sh>
2013-04-26 19:16:29 +0100

    Fix ordering of rm and umount for fusion tools
    Signed-off-by: Ken Barber <ken@bob.sh>

--- templates/CentOS-6.4-x86_64-minimal/vmfusion.sh
+++ templates/CentOS-6.4-x86_64-minimal/vmfusion.sh
@@ -3,5 +3,5 @@ mkdir -p /mnt/cdrom
 mount -o loop /home/veewee/linux.iso /mnt/cdrom
 tar zxvf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
 /tmp/vmware-tools-distrib/vmware-install.pl -d
-rm /home/veewee/linux.iso
 umount /mnt/cdrom
+rm /home/veewee/linux.iso
