f40d4d8bffb740518250b05fff6c76760191673b
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-05-06 15:23:05 +0800

    remove ruby and rubygems source after installing them

--- templates/ubuntu-13.04-server-amd64/ruby.sh
+++ templates/ubuntu-13.04-server-amd64/ruby.sh
@@ -1,16 +1,23 @@
+apt-get -y install libyaml-0-2
+RUBY_VERSION=1.9.3-p392
+
 cd /tmp
 
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz
-tar xvzf ruby-1.9.3-p392.tar.gz
-cd ruby-1.9.3-p392
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-$RUBY_VERSION.tar.gz
+tar xvzf ruby-$RUBY_VERSION.tar.gz
+cd ruby-$RUBY_VERSION
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.9.3-p392
-rm ruby-1.9.3-p392.tar.gz
+rm -rf ruby-$RUBY_VERSION
+rm ruby-$RUBY_VERSION.tar.gz
 
-wget http://production.cf.rubygems.org/rubygems/rubygems-2.0.3.tgz
-tar xzf rubygems-2.0.3.tgz
-cd rubygems-2.0.3
+RUBYGEMS_VERSION=2.0.3
+wget http://production.cf.rubygems.org/rubygems/rubygems-$RUBYGEMS_VERSION.tgz
+tar xzf rubygems-$RUBYGEMS_VERSION.tgz
+cd rubygems-$RUBYGEMS_VERSION
 /opt/ruby/bin/ruby setup.rb
+cd ..
+rm -rf rubygems-$RUBYGEMS_VERSION
+rm rubygems-$RUBYGEMS_VERSION.tgz
