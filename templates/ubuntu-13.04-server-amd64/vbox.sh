4269734b923b398f659d9e56ba1c29fdd7155e9a
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-05-06 13:16:03 +0800

    install libdbus and fix typos on VirtualBox Guest additions

--- templates/ubuntu-13.04-server-amd64/vbox.sh
+++ templates/ubuntu-13.04-server-amd64/vbox.sh
@@ -1,5 +1,11 @@
+# Without libdbus virtualbox would not start automatically after compile
+apt-get -y install --no-install-recommends libdbus-1-3
+
+# Install the VirtualBox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-sh /mnt/VBoxGuestAdditions_$VBOX_VERSION.iso
+VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop $VBOX_ISO /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+rm $VBOX_ISO
