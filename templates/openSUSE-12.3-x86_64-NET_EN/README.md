7ac09c9d4ab60dfc913d8613aa8a8523a7d55cab
Michal Papis <mpapis@gmail.com>
2013-06-14 07:08:36 +0200

    openSUSE-12.3-x86_64-NET_EN

---
+++ templates/openSUSE-12.3-x86_64-NET_EN/README.md
@@ -0,0 +1,5 @@
+# openSUSE definition
+
+## Warning!
+
+During automated steps a lot of crashes happened, in normal usage it works just fine.
---
+++ templates/openSUSE-12.3-x86_64-NET_EN/definition.rb
@@ -0,0 +1,37 @@
+Veewee::Definition.declare({
+  :os_type_id  => 'OpenSUSE_64',
+  :cpu_count   => '2',
+  :memory_size => '1024',
+  :disk_size   => '20480',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :iso_file => "openSUSE-12.3-NET-x86_64.iso",
+  :iso_src  => "http://download.opensuse.org/distribution/12.3/iso/openSUSE-12.3-NET-x86_64.iso",
+  :iso_md5  => "0ef3c2f301b05f52e9c98cff3f799744",
+  :iso_download_timeout => "1000",
+  :boot_wait         => "10",
+  :boot_cmd_sequence => [
+    '<Esc><Enter>',
+    'linux netdevice=eth0 netsetup=dhcp',
+    ' install=http://download.opensuse.org/distribution/12.3/repo/oss/ insecure=1',
+    ' lang=en_US autoyast=http://%IP%:%PORT%/autoinst.xml',
+    ' textmode=1',
+    '<Enter>'
+  ],
+  :kickstart_port    => "7122",
+  :kickstart_timeout => "10000",
+  :kickstart_file    => ["autoinst.xml", "autoinst.xml"],
+  :ssh_login_timeout => "10000",
+  :ssh_user          => "root",
+  :ssh_password      => "vagrant",
+  :ssh_key           => "",
+  :ssh_host_port     => "7222",
+  :ssh_guest_port    => "22",
+  :sudo_cmd     => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "shutdown -P now",
+  :postinstall_files   => ["postinstall.sh", "virtualbox.sh"],
+  :postinstall_timeout => "10000",
+  :hooks => {
+    :before_ssh => Proc.new { sleep 10 }
+  },
+})
