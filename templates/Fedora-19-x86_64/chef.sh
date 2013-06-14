72ebfe8f5431dc7b4ceec76e7db1463eb714bfec
Michal Papis <mpapis@gmail.com>
2013-06-14 18:09:38 +0200

    fix fedora19 chef rubygems issue

--- templates/Fedora-19-x86_64/chef.sh
+++ templates/Fedora-19-x86_64/chef.sh
@@ -12,6 +12,9 @@ yum -y install \
   rubygem-treetop \
   rubygem-uuidtools
 
+# chef 11.4.4 requires rubygems/format which is ruby 1.9 specific and fedora 19 comes with ruby 2.0
+touch /usr/share/rubygems/rubygems/format.rb
+
 # Install Chef
 gem install --no-ri --no-rdoc chef
 
