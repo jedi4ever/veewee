1f801fbe13a9a547b84ece36b0f82a97411268d0
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-06-24 03:55:06 +0800

    install ree and revert rubygems to 1.8.25 (rubygems 2.0.3 breaks chef)

--- templates/ubuntu-13.04-server-amd64-rvm/rvm.sh
+++ templates/ubuntu-13.04-server-amd64-rvm/rvm.sh
@@ -1,4 +1,7 @@
 curl -L get.rvm.io | bash -s stable
 usermod --append --groups rvm vagrant
-/usr/local/rvm/bin/rvm install 2.0.0-p195 
-/usr/local/rvm/bin/rvm alias create default 2.0.0-p195
+RVM=/usr/local/rvm/bin/rvm
+$RVM install ree-1.8.7-2011.12
+$RVM install 2.0.0-p195 
+$RVM alias create default 2.0.0-p195
+$RVM rubygems 1.8.25
