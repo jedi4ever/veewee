1a7aa5cfaaa9ac85d28a18d48bd480550bc07edf
Antek Baranski <abaranski@roblox.com>
2013-07-23 11:56:07 -0700

    Added a rough running? method to status.rb

--- lib/veewee/provider/hyperv/box/helper/status.rb
+++ lib/veewee/provider/hyperv/box/helper/status.rb
@@ -17,12 +17,14 @@ module Veewee
           env.logger.info ("Checking if the VM #{name} #{type} this can take a while because of how remote PowerShell works")
           case type
             when :exists
-              result = powershell_exec ("Get-VM")
+              result = powershell_exec ("Get-VM|Select -Property VMName")
               status = (result.stdout.include? "#{name}") ? true : false
-              env.logger.info("VM #{name} #{type}? #{status}")
             when :running
-              env.logger.info("NOT YET IMPLEMENTED #{type} TESTING")
+              result = powershell_exec ("Get-VM|Select -Property VMName, State")
+              #TODO: Fine tune the check running method
+              status = (result.stdout.include? "#{name}") && result.stdout.include? "Running"? true : false
           end
+          env.logger.info("VM #{name} #{type}? #{status}")
           return status
         end
       end
