6ec5810d71f5da5aa2821fd90e3128d169370bcf
Antek Baranski <abaranski@roblox.com>
2013-07-23 18:09:31 -0700

    Adding additional clean-up steps to destroy such that we remove files as well - not yet enabled

--- lib/veewee/provider/hyperv/box/destroy.rb
+++ lib/veewee/provider/hyperv/box/destroy.rb
@@ -6,6 +6,7 @@ module Veewee
         def destroy(option={})
 
           self.powershell_exec("Remove-VM -Name #{name} -Force")
+          #self.powershell_exec("Get-VM #{name} | %{ Stop-VM -VM $_ -Force; Remove-VM -vm $_ -Force ; Remove-Item -Path $_.Path -Recurse -Force}")
 
         end
 
