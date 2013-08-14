87b4e4345f6c7707dfed67b7c3622a69c1d2d638
Antek Baranski <abaranski@roblox.com>
2013-08-14 14:59:59 -0700

    Cleanup winrm_options for HyperV

--- lib/veewee/provider/hyperv/box/helper/winrm_options.rb
+++ lib/veewee/provider/hyperv/box/helper/winrm_options.rb
@@ -1,16 +1,13 @@
 module Veewee
   module Provider
-    module Hyerpv
+    module Hyperv
       module BoxCommand
 
         def winrm_options
-          port=definition.winrm_host_port
-
           winrm_options={
               :user => definition.winrm_user,
               :pass => definition.winrm_password,
-              :port => port,
-              #            :port => (port.to_i+1).to_s, # debug, by running charles with a reverse proxy
+              :port => definition.winrm_host_port,
               :basic_auth_only => true,
               :timeout => definition.winrm_login_timeout.to_i,
               :operation_timeout => 600 # ten minutes
