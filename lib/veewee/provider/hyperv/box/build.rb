9d86464614ea111c90a0b01e7490755134cc792b
Antek Baranski <abaranski@roblox.com>
2013-08-15 11:45:03 -0700

    We must check for JAVA before we build

--- lib/veewee/provider/hyperv/box/build.rb
+++ lib/veewee/provider/hyperv/box/build.rb
@@ -5,13 +5,15 @@ module Veewee
 
         def build(options={})
 
-          super(options)
-
           if definition.floppy_files
             unless self.shell_exec("java -version").status == 0
               raise Veewee::Error,"This box contains floppyfiles, to create it you require to have java installed or have it in your path"
             end
           end
+
+          super(options)
+
+          #powershell_exec "Remove-VMDvdDrive -VMName #{name} -ControllerNumber 1 -ControllerLocation 1"
 
         end
 
