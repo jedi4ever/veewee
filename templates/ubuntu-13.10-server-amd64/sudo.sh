85e4a7fc1d400445ac650a04d47580e6d66ed714
Trong Tran <trongrg@gmail.com>
2013-10-26 16:12:18 +0700

    Added Ubuntu 13.10-server-amd64 template, Parallels ready
    - Derived from Ubuntu 13.04-server-amd64

---
+++ templates/ubuntu-13.10-server-amd64/sudo.sh
@@ -0,0 +1,5 @@
+groupadd -r admin
+usermod -a -G admin vagrant
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
