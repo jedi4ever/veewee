db8ed5f11218503611eac7fb47361328688c427a
Gregor Zurowski <gregor@zurowski.org>
2014-01-21 18:08:18 -0500

    CentOS 5.10: Remove DHCP leases in clean up script

--- templates/CentOS-5.10-i386/cleanup.sh
+++ templates/CentOS-5.10-i386/cleanup.sh
@@ -6,3 +6,6 @@ rm -rf VBoxGuestAdditions_*.iso
 
 # Remove mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+
+# Remove DHCP leases
+rm /var/lib/dhclient/*.leases
