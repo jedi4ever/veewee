4bcd0be6be60cc0bcc0441a82403118a2c158953
Tom Van Looy <tom@ctors.net>
2014-02-21 20:22:33 +0100

    Add Debian 7.4.0 and 6.0.9 i386 and amd64

---
+++ templates/Debian-7.4.0-amd64-netboot/virtualbox.sh
@@ -0,0 +1,34 @@
+if test -f .vbox_version ; then
+  # The netboot installs the VirtualBox support (old) so we have to remove it
+  if test -f /etc/init.d/virtualbox-ose-guest-utils ; then
+    /etc/init.d/virtualbox-ose-guest-utils stop
+  fi
+
+  rmmod vboxguest
+  aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+
+  # Install dkms for dynamic compiles
+
+  apt-get install -y dkms
+
+  # If libdbus is not installed, virtualbox will not autostart
+  apt-get -y install --no-install-recommends libdbus-1-3
+
+  # Install the VirtualBox guest additions
+  VBOX_VERSION=$(cat .vbox_version)
+  VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
+  mount -o loop $VBOX_ISO /mnt
+  yes|sh /mnt/VBoxLinuxAdditions.run
+  umount /mnt
+
+
+  # Start the newly build driver
+  /etc/init.d/vboxadd start
+
+  # Make a temporary mount point
+  mkdir /tmp/veewee-validation
+
+  # Test mount the veewee-validation
+  mount -t vboxsf veewee-validation /tmp/veewee-validation
+
+fi
