59ff67390cc5c7cc458f088c3d8f0ed966b89a07
Gregor Zurowski <gregor@zurowski.org>
2014-04-19 09:06:53 -0400

    [Minor] Force delete udev rules file to suppress warnings.
    Signed-off-by: Gregor Zurowski <gregor@zurowski.org>

--- templates/ubuntu-14.04-server-amd64/cleanup.sh
+++ templates/ubuntu-14.04-server-amd64/cleanup.sh
@@ -7,7 +7,7 @@ echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
 echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
