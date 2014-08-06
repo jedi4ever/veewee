34cc4af73ee772123ae405059f39cdff63bf23d8
Gordon Gao <ghprince@gmail.com>
2014-08-07 01:04:42 +0800

    Finetune Solaris 10 template: - move :hwvirtex into :vm_options - correct os_type_id - use curl/bash method to install chef in order to prevent gem compile   issue - modify instruction to use `veewee box export` instead of raw Vagrant   commands

--- templates/solaris-10-ga-x86/chef.sh
+++ templates/solaris-10-ga-x86/chef.sh
@@ -1,10 +1,7 @@
 # Chef needs these
 /opt/csw/bin/pkgutil -y -i CSWgmake
 /opt/csw/bin/pkgutil -y -i CSWgcc4g++ CSWgcc4core
+/opt/csw/bin/pkgutil -y -i CSWcurl
 
-# These are needed to get a compiler working
-# Mainly because chef depends on compiling some native gems
-PATH=/opt/csw/bin:/opt/csw/gnu/:/opt/csw/gcc4/bin:$PATH
-export PATH
-
-/opt/csw/bin/gem install chef  --no-ri --no-rdoc
+# Install Chef
+curl -L https://www.opscode.com/chef/install.sh | bash
