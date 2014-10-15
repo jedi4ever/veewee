dc18a60263755ce02fd4d1ec60bcf6dfc9fdfe39
Michal Papis <mpapis@gmail.com>
2014-10-15 18:07:02 +0200

    extra warning for libwirt error, closes #996

--- lib/veewee/provider/kvm/box/up.rb
+++ lib/veewee/provider/kvm/box/up.rb
@@ -6,6 +6,11 @@ module Veewee
         def up(options={})
           matched_servers=@connection.servers.all(:name => name)
           matched_servers.first.start unless matched_servers.nil?
+        rescue Libvirt::Error => e
+          warn "
+Libvirt Error! Make sure your user has permissions to run anything.
+"
+          raise e
         end
 
       end # End Module
