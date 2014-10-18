f53224e7a3175db5f875e9e341b8e7ab7b62cbd8
Sebastian Wendel <packages@sourceindex.de>
2014-10-18 05:13:36 +0200

    added template for the kali linux distribution

---
+++ templates/kali-1.0.9a-amd64/vagrant.sh
@@ -0,0 +1,16 @@
+# Set up Vagrant.
+
+date > /etc/vagrant_box_build_time
+
+# Install vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+curl -Lo /home/vagrant/.ssh/authorized_keys \
+  'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# Customize the message of the day
+echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
+
+# Install NFS client
+apt-get -y install nfs-common
