1b3b8629b168630a27c7d8ac621f5d506b7e96c1
Michal Papis <mpapis@gmail.com>
2014-10-24 08:15:40 +0200

    use full word for directory in definition to be more clear about what has to be done

--- templates/openSUSE-13.1/definition.rb
+++ templates/openSUSE-13.1/definition.rb
@@ -1,5 +1,5 @@
 #
-# change opensuse_64_dvd to one of configurations in *.yml in this dir
+# change opensuse_64_dvd to one of configurations in *.yml in this directory
 # use the yml files to configure
 #
 Veewee::Definition.declare_yaml('definition.yml', 'opensuse_64_dvd.yml')
