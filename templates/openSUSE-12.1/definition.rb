32986d6942879b2c5a6e19bfa35a4778b89deda3
Michal Papis <mpapis@gmail.com>
2014-10-24 08:16:40 +0200

    move openSUSE-12.1-DVD+NET-i586 to openSUSE-12.1 - extract yaml files for configuration

---
+++ templates/openSUSE-12.1/definition.rb
@@ -0,0 +1,5 @@
+#
+# change opensuse_64_dvd_en to one of configurations in *.yml in this directory
+# use the yml files to configure
+#
+Veewee::Definition.declare_yaml('definition.yml', 'opensuse_64_dvd_en.yml')
