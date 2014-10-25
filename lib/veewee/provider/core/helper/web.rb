ccb887ad5c33d7be722c4350e0363917420ead7c
Michal Papis <mpapis@gmail.com>
2014-10-25 22:52:14 +0200

    fix template kickstart files, fix timeout for serving files with webrick

--- lib/veewee/provider/core/helper/web.rb
+++ lib/veewee/provider/core/helper/web.rb
@@ -6,80 +6,78 @@ module Veewee
 
         include WEBrick
 
-        module Servlet
-
-          class FileServlet < WEBrick::HTTPServlet::AbstractServlet
-            attr_reader :ui, :threaded
-
-            def initialize(server,localfile,ui,threaded)
-              super(server)
-              @server=server
-              @localfile=localfile
-              @ui=ui
-              @threaded=threaded
-            end
-
-            def do_GET(request,response)
-              response['Content-Type']='text/plain'
-              response.status = 200
-              content = File.open(@localfile, "r").read
-              response.body =
-              case File.extname(@localfile)
-              when ".erb"
-                ui.info "Rendering and serving file #{@localfile}"
-                ERB.new(content).result(binding)
-              else
-                ui.info "Serving file #{@localfile}"
-                content
-              end
-              if not @threaded
-                ui.info "Shutting down for #{@localfile}"
-                @server.shutdown
-              end
-            end
-          end
-        end
-
         module Web
-          def wait_for_http_request(filename, urlname, options) # original blocking
-            server_for_http_request(filename, urlname, options) do |server|
-              server.start
+          def wait_for_http_request(filename, urlname, options) # thread with timeout
+            thread  = allow_for_http_request(filename, urlname, options)
+            timeout = options[:timeout] || 60
+            thread.join(timeout) or begin
+              thread.kill
+              raise "File #{filename.inspect} was not requested in #{timeout} seconds, are you using firewall blocking connections to port: #{options[:port]}?"
             end
           end
 
           def allow_for_http_request(filename, urlname, options) # start in new thread
-            server_for_http_request(filename, urlname, options.merge({:threaded => false})) do |server|
-              t = Thread.new { server.start }
-              t.abort_on_exception = true
+            thread = Thread.new do
+              server_for_http_request(filename, urlname, options.merge({:threaded => false}))
+            end
+            thread.abort_on_exception = true
+            trap("INT") { thread.kill }
+            thread
+          end
+
+        private
+
+          def server_for_http_request(filename, urlname, options, &block)
+            read_content(filename)
+            initialize_server(options[:port])
+            mount_file(urlname, !options[:threaded])
+            @server.start
+          ensure
+            server_shutdown
+          end
+
+          def read_content(filename)
+            ui.info "Reading content #{filename}"
+            @content = File.open(filename, "r").read
+            if File.extname(filename) == ".erb"
+              ui.info "Evaluating template #{filename}"
+              @content = ::ERB.new(@content).result(binding)
             end
           end
 
-          def server_for_http_request(filename, urlname, options, &block)
+          def initialize_server(port)
             # Calculate the OS equivalent of /dev/null , on windows this is NUL:
             # http://www.ruby-forum.com/topic/115472
             fn = test(?e, '/dev/null') ? '/dev/null' : 'NUL:'
             webrick_logger = WEBrick::Log.new(fn, WEBrick::Log::INFO)
 
-            timeout = options[:timeout] || 60
-            Timeout.timeout(timeout) do
-              server =
-              ::WEBrick::HTTPServer.new(
-                :Port => options[:port],
-                :Logger => webrick_logger,
-                :AccessLog => webrick_logger
-              )
-              env.logger.debug("mounting file #{urlname}")
-              server.mount("#{urlname}", Veewee::Provider::Core::Helper::Servlet::FileServlet, filename, ui, options[:threaded])
-              trap("INT"){
-                server.shutdown
-                ui.info "Stopping webserver"
-                exit
-              }
-              yield server
+            @server =
+            ::WEBrick::HTTPServer.new(
+              :Port => port,
+              :Logger => webrick_logger,
+              :AccessLog => webrick_logger,
+            )
+          end
+
+          def mount_file(urlname, one_file_only = true)
+            urlname = urlname[0..-5] if File.extname(urlname)  == ".erb"
+
+            @server.mount_proc(urlname) do |request, response|
+              ui.info "Serving content for #{urlname}"
+              response['Content-Type']='text/plain'
+              response.status = 200
+              response.body   = @content
+              server_shutdown if one_file_only
             end
-          rescue Timeout::Error
-            server.shutdown unless server.nil?
-            raise "File #{filename.inspect} was not requested in #{timeout}seconds, are you using firewall blocking connections to port: #{options[:port]}?"
+          end
+
+          def server_shutdown
+            if @server
+              ui.info "Stopping webserver"
+              yield @server if block_given?
+              @server.shutdown
+              @server = nil
+            end
           end
 
         end #Class
