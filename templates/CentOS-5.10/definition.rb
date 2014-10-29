3e3dc8fc82050e98c20149d5e413f2c16ec67059
Michal Papis <mpapis@gmail.com>
2014-10-29 18:10:04 +0100

    extract centos_64_dvd.yml for 5.10, preparing to merge 32_dvd

--- templates/CentOS-5.10/definition.rb
+++ templates/CentOS-5.10/definition.rb
@@ -2,4 +2,4 @@
 # change centos_64_dvd to one of configurations in *.yml in this directory
 # use the yml files to configure
 #
-Veewee::Definition.declare_yaml('definition.yml')
+Veewee::Definition.declare_yaml('definition.yml', "centos_64_dvd.yml")
