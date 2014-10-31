e4dfbea99135a8ae92e60a831dd23dc4c3249067
Michal Papis <mpapis@gmail.com>
2014-10-31 21:54:57 +0100

    add teste for kickstarter erb files

---
+++ test/veewee/provider/core/helper/web_test.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require 'veewee'
+
+class TestVeeweeBuild < Test::Unit::TestCase
+  def setup
+    @definition_dir = File.expand_path("../../../../../definitions", __FILE__)
+    @definition_name = "erb_definition"
+    ve = Veewee::Environment.new({ :definition_dir =>  @definition_dir })
+    @box = ve.providers["virtualbox"].get_box(@definition_name)
+  end
+
+  def test_box_1_build
+    assert_equal(
+      "\
+#!/bin/bash
+
+echo \"Testing one Linux26\"
+
+echo DVD
+
+",
+      @box.send(:read_content, File.join(@definition_dir, @definition_name, "autorun0.erb"))
+    )
+  end
+end
