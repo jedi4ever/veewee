0876fda32ae8ff059ec95c82c08cbec42ec5489b
Michal Papis <mpapis@gmail.com>
2014-11-02 21:39:16 +0100

    move CentOS-6.5 x86_64-minimal to generic yaml based template

---
+++ templates/CentOS-6.5/definition.rb
@@ -0,0 +1,5 @@
+#
+# change centos_64_dvd to one of configurations in *.yml in this directory
+# use the yml files to configure
+#
+Veewee::Definition.declare_yaml('definition.yml', "centos_64_minimal.yml")
