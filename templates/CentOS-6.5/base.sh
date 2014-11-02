81a2c9e40fdd1ff48eb0a0289fdd5bf35e31811d
Michal Papis <mpapis@gmail.com>
2014-11-02 22:54:12 +0100

    merge CentOS-6.5 with i386-minimal

--- templates/CentOS-6.5/base.sh
+++ templates/CentOS-6.5/base.sh
@@ -5,7 +5,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+baseurl=https://download.fedoraproject.org/pub/epel/6/\$basearch
 enabled=1
 gpgcheck=0
 EOM
