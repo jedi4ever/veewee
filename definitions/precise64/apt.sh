2ae980195c412559a4cdfa065417eef8a7797e02
Ward Fisher <wfisher@ucar.edu>
2015-02-23 12:36:57 -0700

    Added definitions for 32, 64-bit precise pangolin (ubuntu 12.04.4)

---
+++ definitions/precise64/apt.sh
@@ -0,0 +1,8 @@
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+apt-get -y install vim
+apt-get -y install dkms
+apt-get -y install nfs-common
+apt-get -y install rsync
