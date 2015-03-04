1f256015c5c3486127c23ea967adc25a1ef44f4a
Ward Fisher <wfisher@unidata.ucar.edu>
2015-03-04 08:18:09 -0700

    Added centos 6.5, 7 definitions.

---
+++ definitions/centos7-x64/cleanup.sh
@@ -0,0 +1,7 @@
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
+rm -rf VBoxGuestAdditions_*.iso
+
+# Remove traces of mac address from network configuration
+sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-enp0s3
---
+++ definitions/centos7-x64/definition.rb
@@ -0,0 +1,38 @@
+Veewee::Session.declare({
+  :cpu_count => '1',
+  :memory_size=> '480',
+  :disk_size => '10140',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :os_type_id => 'RedHat6_64',
+  :iso_file => "CentOS-7.0-1406-x86_64-NetInstall.iso",
+  :iso_src => "http://mirror.nextlayer.at/centos/7.0.1406/isos/x86_64/CentOS-7.0-1406-x86_64-NetInstall.iso",
+  :iso_md5 => "96de4f38a2f07da51831153549c8bd0c",
+  :iso_download_timeout => 1000,
+  :boot_wait => "10",
+  :boot_cmd_sequence => [
+    '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => 300,
+  :kickstart_file => "ks.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "veewee",
+  :ssh_password => "veewee",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
+  :postinstall_files => [
+    "base.sh",
+    #"chef.sh",
+    #"puppet.sh",
+    "vagrant.sh",
+    "virtualbox.sh",
+    #"vmfusion.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => 10000
+})
