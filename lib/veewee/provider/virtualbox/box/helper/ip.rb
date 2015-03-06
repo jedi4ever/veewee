36c3a46fad0cbd4919485a8dcf4ae4453d89d0b1
Catalin Trifu <ctrifu@gmail.com>
2015-03-06 22:41:40 +0200

    Fix for VBox NAT interface address

--- lib/veewee/provider/virtualbox/box/helper/ip.rb
+++ lib/veewee/provider/virtualbox/box/helper/ip.rb
@@ -5,7 +5,11 @@ module Veewee
 
         # http://www.virtualbox.org/manual/ch09.html#idp13716288
         def host_ip_as_seen_by_guest
-          "10.0.2.2"
+          # as per definition in virtualbox 
+          # we need to add 1 because vboxmanage starts counting from 1 for display,
+          # but counts from 0 when actually assigning the IP
+          host_ip = "10.0.#{self.natinterface.to_i + 1}.2"
+          return host_ip # "10.0.2.2"
         end
 
         # Get the IP address of the box
