31c9e722b74efdfe78fc8224886a2393e3b385bd
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-09-23 15:00:36 +0200

    correct EPEL
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- templates/CentOS-7.0-1406-x86_64-netinstall/base.sh
+++ templates/CentOS-7.0-1406-x86_64-netinstall/base.sh
@@ -2,13 +2,15 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
-enabled=1
-gpgcheck=0
-EOM
+yum install -y epel-release
+
+#cat > /etc/yum.repos.d/epel.repo << EOM
+#[epel]
+#name=epel
+#baseurl=http://download.fedoraproject.org/pub/epel/7/x86_64/\$basearch
+#enabled=1
+#gpgcheck=0
+#EOM
 
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils bzip2
 
