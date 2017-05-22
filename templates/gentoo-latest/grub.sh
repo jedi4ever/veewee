c0dad6b378032617ea7e0eda7bb28b1166e6a602
Courtney Miles <courtney@milesasylum.com>
2017-05-22 17:29:56 +1000

    Corrections for installing Grub

--- templates/gentoo-latest/grub.sh
+++ templates/gentoo-latest/grub.sh
@@ -20,9 +20,7 @@ chroot "$chroot" sed -i "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"net.ifname
 chroot "$chroot" /bin/bash <<DATAEOF
 source /etc/profile && \
 env-update && \
-grep -v rootfs /proc/mounts > /etc/mtab && \
-mkdir -p /boot/grub2 && \
-ln -sf /boot/grub2 /boot/grub && \
-grub2-mkconfig -o /boot/grub/grub.cfg && \
-grub2-install --no-floppy /dev/sda
+mkdir -p /boot/grub && \
+grub-mkconfig -o /boot/grub/grub.cfg && \
+grub-install --no-floppy /dev/sda
 DATAEOF
