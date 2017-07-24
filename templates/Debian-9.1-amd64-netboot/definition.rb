4812b0e08f65dd482a83ae8aaffd82966cd8befd
Alexander Reitzel <funtimecoding@gmail.com>
2017-07-24 15:40:51 +0200

    add template for Debian 9.1, copy from 8.2, replace image locator and checksum

---
+++ templates/Debian-9.1-amd64-netboot/definition.rb
@@ -0,0 +1,5 @@
+#
+# change debian_64_netinst to one of configurations in *.yml in this directory
+# use the yml files to configure
+#
+Veewee::Definition.declare_yaml('definition.yml', "debian_64_net.yml")
